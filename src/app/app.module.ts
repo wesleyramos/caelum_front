34704dae0643ea4054aa7eeb375814abbe5bd42f
Wesley <wesley.goncalves@furukawaelectric.com>
2020-08-31 17:19:42 -0300

    exerc 4_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent //HeaderComponent aqui!
   ],
   imports: [
     BrowserModule

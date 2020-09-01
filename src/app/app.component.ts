7d87450a5b35707fbed2fe6d09858f99a023f1da
Wesley <wesley.goncalves@furukawaelectric.com>
2020-09-01 15:34:00 -0300

    exerc 10.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   private _isNewEmailFormOpen = false;
+
+  emailList = [];
+  email = {
+    destinatario: '',
+    assunto: '',
+    conteudo: ''
+  }
 
   get isNewEmailFormOpen() {
     return this._isNewEmailFormOpen;
   }
   toggleNewEmailForm() {
     this._isNewEmailFormOpen = !this.isNewEmailFormOpen
+  }
+
+  handleNewEmail(event: Event) {
+    event.preventDefault();
+    this.emailList.push(this.email)
+    this.email = {
+      destinatario: '',
+      assunto: '',
+      conteudo: ''
+    }
   }
 
 }

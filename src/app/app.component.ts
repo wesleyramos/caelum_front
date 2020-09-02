39278c1a42eb86ae2c2149c9bfbac24a1b02408d
Wesley <wesley.goncalves@furukawaelectric.com>
2020-09-02 14:48:46 -0300

    exerc 15.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,12 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<router-outlet></router-outlet>',
+  styles: []
 })
 export class AppComponent {
 
-  private _isNewEmailFormOpen = false;
 
-  emailList = [];
-  email = {
-    destinatario: '',
-    assunto: '',
-    conteudo: ''
-  }
-
-  get isNewEmailFormOpen() {
-    return this._isNewEmailFormOpen;
-  }
-  toggleNewEmailForm() {
-    this._isNewEmailFormOpen = !this.isNewEmailFormOpen
-  }
-
-  handleNewEmail(formEmail: NgForm) {
-    if (formEmail.invalid) return;
-    this.emailList.push(this.email)
-    this.email = {
-      destinatario: '',
-      assunto: '',
-      conteudo: ''
-    }
-    formEmail.reset();
-  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 
 import { FormsModule } from "@angular/forms";
+import { CaixaDeEntradaComponent } from './modules/caixa-de-entrada/caixa-de-entrada.component';
+import { LoginComponent } from './modules/login/login.component';
+import { CadastroComponent } from './modules/cadastro/cadastro.component';
+import { ModuloRoteamento } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent //HeaderComponent aqui!
+    HeaderComponent,
+    CaixaDeEntradaComponent,
+    LoginComponent,
+    CadastroComponent //HeaderComponent aqui!
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ModuloRoteamento
   ],
   providers: [],
   bootstrap: [AppComponent]

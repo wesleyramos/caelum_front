8d5134f2fba4de5113f28a54d59f2a282468a2c5
Wesley <wesley.goncalves@furukawaelectric.com>
2020-09-09 15:44:20 -0300

    exerc 44.2

--- src/app/modules/caixa-de-entrada/caixa-de-entrada.component.ts
+++ src/app/modules/caixa-de-entrada/caixa-de-entrada.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { EmailService } from '../../services/email.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'cmail-caixa-de-entrada',
@@ -17,8 +18,26 @@ export class CaixaDeEntradaComponent {
     assunto: '',
     conteudo: ''
   }
+  mensagemErro = '';
 
   constructor(private emailService: EmailService) { }
+
+  ngOnInit() {
+    this.emailService
+      .listar()
+      .subscribe(
+        //next
+        lista => {
+          this.emailList = lista;
+        },
+        //erro
+        (responseError: HttpErrorResponse) => {
+          this.mensagemErro = responseError.message;
+          console.log("Deu ruim");
+        },
+        //complete (finally)
+      )
+  }
 
   get isNewEmailFormOpen() {
     return this._isNewEmailFormOpen;
--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -34,4 +34,25 @@ export class EmailService {
                     }
                 ))
     }
+
+    //codigo anterior omitido
+    listar() {
+        return this.http
+            .get(this.api, { headers: this.cabecalho })
+            .pipe<Email[]>(
+                map(
+                    (response: any[]) => {
+                        return response
+                            .map(
+                                emailApi => new Email({
+                                    destinatario: emailApi.to,
+                                    assunto: emailApi.subject,
+                                    conteudo: emailApi.content,
+                                    dataDeEnvio: emailApi.createdAt
+                                })
+                            )
+                    }
+                )
+            )
+    }
 }
